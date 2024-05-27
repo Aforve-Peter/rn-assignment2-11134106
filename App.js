560b527fc200a1c847fe3c6b3d03f9d243f3c65f
Aforve-Peter <pkaforve@st.ug.edu.gh>
2024-05-27 15:41:02 +0000

    fontsize update

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My name is Aforve Peter</Text>
+      <Text style={{fontSize:24}}>My name is Aforve Peter</Text>
       <StatusBar style="auto" />
     </View>
   );
