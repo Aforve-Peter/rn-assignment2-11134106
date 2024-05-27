fc1c0e5321ed3d670e895a69da958679771ca1c9
Aforve-Peter <pkaforve@st.ug.edu.gh>
2024-05-27 15:35:47 +0000

    My name

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>My name is Aforve Peter</Text>
       <StatusBar style="auto" />
     </View>
   );
